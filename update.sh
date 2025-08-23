25a6bf2152f2c819b65f761f3c36f1b95e857c48
DaShaun <dashaun@dashaun.com>
2025-08-22 21:33:37 -0500

    remove springdoc-openapi

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-advisor mapping build -r='https://github.com/Azure/azure-sdk-for-java'
-advisor mapping build -r='https://github.com/springdoc/springdoc-openapi/'
+rm -rf .advisor/tmp
+advisor mapping build -r='https://github.com/Azure/azure-sdk-for-java' -c com.azure:azure-identity
+#advisor mapping build -r='https://github.com/springdoc/springdoc-openapi/'
